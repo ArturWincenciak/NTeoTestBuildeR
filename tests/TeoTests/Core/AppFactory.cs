8d644369f6c6055bff9444198b173f95dd288c5f
TeoVincent Artur Wincenciak <artur.wincenciak@gmail.com>
2024-04-16 18:45:43 +0200

    Testcontainer with DB for testing

--- tests/TeoTests/Core/AppFactory.cs
+++ tests/TeoTests/Core/AppFactory.cs
@@ -1,6 +1,34 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
 using NTeoTestBuildeR;
+using Testcontainers.PostgreSql;
 
 namespace TeoTests.Core;
 
-public sealed class AppFactory : WebApplicationFactory<Program>;
+public sealed class AppFactory : WebApplicationFactory<Program>
+{
+    private readonly PostgreSqlContainer _postgresContainer = BuildPostgres();
+
+    private static PostgreSqlContainer BuildPostgres() =>
+        new PostgreSqlBuilder()
+            .WithImage("postgres")
+            .WithDatabase("teo-app")
+            .WithAutoRemove(autoRemove: true)
+            .WithCleanUp(cleanUp: true)
+            .Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder) => builder
+        .ConfigureAppConfiguration(ConfigureTestcontainers);
+
+    private void ConfigureTestcontainers(IConfigurationBuilder configurationBuilder)
+    {
+        _postgresContainer.StartAsync().Wait();
+        var connectionString = _postgresContainer.GetConnectionString();
+
+        configurationBuilder.AddInMemoryCollection(new KeyValuePair<string, string?>[]
+        {
+            new(key: "ConnectionStrings:TeoTodoApp", connectionString)
+        });
+    }
+}
